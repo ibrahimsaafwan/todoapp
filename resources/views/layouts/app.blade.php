cbcadbacb95d34f7d333b84dedd736a865482f42
ibrahimsaafwan <ibrahimsaafwan@gmail.com>
2025-03-13 06:27:14 -0400

    Final Change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <title>To-Do List App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
+
 </head>
 
 <body>
